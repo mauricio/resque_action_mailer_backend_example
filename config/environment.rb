a7fd7bd4005bcb38f6781aab697b0db6810cb6dc
Maurício Linhares <mauricio.linhares@gmail.com>
2010-08-19 16:46:42 -0300

    Updating to the latest vesion of the gem

--- config/environment.rb
+++ config/environment.rb
@@ -21,7 +21,7 @@ Rails::Initializer.run do |config|
   # config.gem "aws-s3", :lib => "aws/s3"
 
   config.gem 'resque'
-  config.gem 'resque_action_mailer_backend'
+  config.gem 'resque_action_mailer_backend', :version => '0.0.2'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
